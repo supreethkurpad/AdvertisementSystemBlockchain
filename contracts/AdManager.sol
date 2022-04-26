dbe9fc8c716b20b41345e6c4c9754499752f34a8
supreethkurpad <supreethkurpad@gmail.com>
2022-04-26 11:55:05 +0530

    Add Create User and Ad function

--- contracts/AdManager.sol
+++ contracts/AdManager.sol
@@ -1,5 +1,34 @@
 pragma solidity ^0.5.0;
 
 contract AdManager {
-    uint32 public adCount = 0;
+    uint public adCount = 0;
+    uint public userCount = 0;
+
+    mapping(uint => User) public activeUsers;
+
+    struct Ad {
+        uint id;
+        string content;
+        uint pending_count;
+    }
+
+    struct User {
+        uint user_id;
+        uint active_ads;
+        mapping(uint => Ad) advertisements;
+    }
+
+    function createUser() public {
+        userCount++;
+        User memory new_user = User({user_id : userCount, active_ads : 0});
+        activeUsers[userCount] = new_user;
+    }
+
+    function createAd(uint user_id, string memory _content, uint _count) public {
+        adCount++;
+        Ad memory new_ad = Ad(adCount, _content, _count);
+        uint count = ++activeUsers[user_id].active_ads;
+        activeUsers[user_id].advertisements[count] = new_ad;
+    }
+
 }
